938fbacdf45ea3a7788b80350ef632f5c58c7b00
wbeimar <wbeimarmonsalve33@gmail.com>
2025-03-27 12:42:43 -0500

    Aprendiendo el concepto de monohilos en Node.js

---
+++ conceptos/monohilo.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo¡")
+
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++
+
+    if (i === 5) {
+        onslotchange.log("Forzando error");
+        var a = 3 + z;
+    }
+}, 1000);
+console.log("Segunda instruccion");
