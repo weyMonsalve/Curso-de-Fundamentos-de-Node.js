04074acc80a1a049f2d630c12d964c8f431f2fbf
wbeimar <wbeimarmonsalve33@gmail.com>
2025-04-03 10:10:24 -0500

    aprendiendo file system

---
+++ modulos/fs.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const { dirname } = require('path');
+
+function leer(ruta, cb) {
+    fs.readFile(ruta, (err, data) => {
+        cb(data.toString());
+    })
+}
+
+
+function escribir(ruta, contenido, cb) {
+    fs.writeFile(ruta, contenido, function(err) {
+        if (err) {
+            console.error('No he podido escribrlo', err)
+        } else {
+            console.log('Se ha escrito correctamente');
+        }
+    });
+}
+
+
+function borrar(ruta, cb) {
+    fs.unlink(ruta, cb);
+}
+
+//
+
+escribir(__dirname + '/archivo1.txt', 'Soy un archivo nuevo', console.log);
+
+leer(__dirname + '/archivo1.txt', console.log);
+
+borrar(__dirname + '/archivo1.txt', console.log)
