9de2975120d9b4894e9ae6cc959e6dfd98498b66
wbeimar <wbeimarmonsalve33@gmail.com>
2025-04-03 08:30:00 -0500

    modulos con varibles globales

---
+++ modulos/globales.js
@@ -0,0 +1,14 @@
+// let i = 0;
+// let intervalo = setInterval(function() {
+//     console.log('Hola');
+//     if (i === 3) {
+//         clearInterval(intervalo);
+//     }
+//     i++;
+// }, 1000);
+
+
+
+setImmediate(function() {
+    console.log('Hola');
+});
